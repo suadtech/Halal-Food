b23b4f99f4dc7f55e6d540eadb5e30ee47b32717
suad alhodifi <suadtech1@gmail.com>
2025-03-25 10:23:31 +0000

    1

--- halal_food/halal_food/urls.py
+++ halal_food/halal_food/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path , include
+from halal_food import views as index_views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('bookings.urls')),
+
 ]
